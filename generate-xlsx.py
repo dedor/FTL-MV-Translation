07e5f3a2da9cf60e01abd12dcff05683dff781ec
marona <marona42@naver.com>
2022-03-16 18:47:17 +0900

    second iteration

--- generate-xlsx.py
+++ generate-xlsx.py
@@ -5,17 +5,23 @@ import openpyxl as xl
 
 
 
-tag_filter = {'text','visitedTooltip','undiscoveredTooltip','title','short','desc','tooltip','power','buttonText'}
+tag_filter = {'text', 'undiscoveredTooltip', 'power', 'desc', 'visitedTooltip', 'unvisitedTooltip', 'title', 'flavorType', 'short',"header","secretName","secretDescription","class"}
 
-def write_seg(name,text):
-    if text == None or text == '\t': return
-    xls.active.append([name,text])
+def write_seg(name,text,tag,attrib):
+    if text == None or text.strip() == "" or text.isnumeric(): return ""#exclude numbers and blanks
+    if tag not in tag_filter: return ""   #filter by tags
+
+    if 'name' in attrib: name=attrib['name']
+    else: name=''
+    xls.active.append([text,"",name])   #["source","target","context","comments"]
+    #print(text,name)
+    return name
 
 def extract_text(root,segname):
-    if root.tag in tag_filter: write_seg(segname,root.text)
+    segname = write_seg(segname,root.text,root.tag,root.attrib)
     for node in root:
-        if 'name' in node.attrib: segname=node.attrib['name']
-        if node.tag in tag_filter: write_seg(segname,node.text)
+        #print(f"{node.tag} : {node.text}")
+        segname = write_seg(segname,node.text,node.tag,node.attrib)
 
         nodecnt=0
         for children in node:
@@ -35,9 +41,18 @@ for file in os.listdir("./orig"):
     if fnmatch.fnmatch(file,"*.xml*"):
         print(f"parsing {file}...")
         xls=xl.workbook.Workbook()
-        xls.active.append(["name","text"])
-        xml=et.parse(f"./orig/{file}")
-        xml_root = xml.getroot()
-        #if fnmatch.fnmatch(file,"events*.xml*"):
-        extract_text(xml_root,"")
-        xls.save(filename=f'./xls/{file}.xlsx')
+        xls.active.append(["source","target","context","comments"])
+        try:
+            xml=et.parse(f"./orig/{file}")
+            xml_root = xml.getroot()
+            #if fnmatch.fnmatch(file,"events*.xml*"):
+            #iter_node(xml_root)
+            extract_text(xml_root,"")
+            if xls.active.max_row > 1:
+                xls.save(filename=f'./xls/{file}.xlsx')
+            else:
+                print(f"WARNING: no texts in {file}, did not write xlsx.")
+        except et.ParseError as e:
+            print(f"ERROR: cannot parse {file}: {e.msg}")
+            xls=None
+
